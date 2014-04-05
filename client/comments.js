660d1671af093d872040f8adcd4c66549697820a
Prasath <n.prasath.002@gmail.com>
2014-04-05 19:11:58 +0530

    implementing comment

--- client/comments.js
+++ client/comments.js
@@ -1,1 +1,38 @@
+//LIST THREADS
+Template..posts = function () {
+	return Posts.find({}, { sort: { time: -1 }});
+}
 
+Template.post.events({
+	'click .clickable': function () {
+		Posts.update(this._id, {$inc: {
+			time: Date.now()
+		}});
+	}
+});
+
+
+//ADD NEW POST
+Template.input.events = {
+	'keydown input#newpost' : function (event) {
+		if (event.which == 13) { // 13 is the enter key event
+
+			//CHECK USER AUTHENTICATION
+			var user = 'User';
+
+			var newpost = document.getElementById('newpost');
+
+			if (newpost.value != '') {
+
+				//ADD DATA TO MONGODB
+				Posts.insert({
+					user: user,
+					content: newpost.value,
+					time: Date.now()
+				});
+
+				newpost.value = '';
+			}
+		}
+	}
+}
