22e6ffa4b29ea1e8c18d5e815d9d48250a628dd9
Jeyarathnem Jeyachanthuru <jeyachanthuruj@gmail.com>
2014-04-05 21:58:43 +0530

    added

--- client/thread.js
+++ client/thread.js
@@ -1,39 +1,46 @@
+var clicked_post = 'q527t2p2T4czEQ7EK';
+
 //LIST THREADS
-Template.threads.posts = function () {
-	return Posts.find({}, { sort: { time: -1 }});
+Template.threads.helpers({
+    posts : function () {
+	    return Posts.find({}, { sort: { time: -1 }});
+    },
+    events : {
+        'click .thread': function () {
+            clicked_post = this._id;
+
+            console.log(clicked_post)
+
+
+        },
+        'keydown input#newpost':function(event){
+            if (event.which == 13) { // 13 is the enter key event
+
+                //CHECK USER AUTHENTICATION
+                var user = 'User';
+
+                var newpost = document.getElementById('newpost');
+
+                if (newpost.value != '') {
+
+                    //ADD DATA TO MONGODB
+                    Posts.insert({
+                        user: user,
+                        message: newpost.value,
+                        time: Date.now()
+                    });
+
+                    newpost.value = '';
+                }
+            }
+        }
+    }
+})
+
+
+Template.postfull.post = function() {
+    if(clicked_post){
+        return Posts.find({_id : clicked_post});
+    }
 }
 
-Template.threads.events({
-	'click .clickable': function () {
-		/*Posts.update(this._id, {$inc: {
-			time: Date.now()
-		}});*/
-
-	}
-});
-
-
-//ADD NEW POST
-Template.input.events = {
-	'keydown input#newpost' : function (event) {
-		if (event.which == 13) { // 13 is the enter key event
-
-			//CHECK USER AUTHENTICATION
-			var user = 'User';
-
-			var newpost = document.getElementById('newpost');
-
-			if (newpost.value != '') {
-
-				//ADD DATA TO MONGODB
-				Posts.insert({
-					user: user,
-                    message: newpost.value,
-					time: Date.now()
-				});
-
-				newpost.value = '';
-			}
-		}
-	}
-}
